4bdce377ecc531658119eb93f49c802ad01bf136
ViNghiaDat <Datb1910207@student.ctu.edu.vn>
2023-09-20 19:24:57 +0700

    created contactbook frontend

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm @submit:contact="createContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Tạo liên hệ thành công nghe";
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    },
+    created() {
+        this.message = "";
+    },
+};
+</script>
+  
